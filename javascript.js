7f92b5c24333033fb9dc2eb366b20f17eb4269ad
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-04-12 21:40:02 +0200

    Add displayResult logic

--- javascript.js
+++ javascript.js
@@ -18,12 +18,10 @@ function Gameboard() {
 
     // Player places token
     const placeMark = (row, col, mark) => {
-        const realRow = row - 1;
-        const realCol = col - 1;
-        if (!validCell(realRow, realCol)) {
+        if (!validCell(row, col)) {
             return false;
         }
-        board[realRow][realCol] = mark;
+        board[row][col] = mark;
         return true;
     }
     
@@ -98,33 +96,34 @@ function Gamecontroller() {
         console.log(`It s ${activePlayer.name}s turn: `);
     }
 
-    function checkWinner() {
+    const checkWinner = () => {
         const b = board.getBoard();
         for (let i=0; i<3; i++) {
             // Check rows
-            if(b[i][0] === b[i][1] && b[i][1] === b[i][2] && b[i][0] != "0") {
+            if(b[i][0] === b[i][1] && b[i][1] === b[i][2] && b[i][0] != " ") {
                 return true;
             }
             // Check columns
-            if(b[0][i] === b[1][i] && b[1][i] === b[2][i] && b[0][i] != "0") {
+            if(b[0][i] === b[1][i] && b[1][i] === b[2][i] && b[0][i] != " ") {
                 return true;
             }
         }
         // Check diagonals
-        if(b[0][0] === b[1][1] && b[1][1] === b[2][2] && b[0][0] != "0") {
+        if(b[0][0] === b[1][1] && b[1][1] === b[2][2] && b[0][0] != " ") {
             return true;
         }
-        if(b[0][2] === b[1][1] && b[1][1] === b[2][0] && b[1][1] != "0") {
+        if(b[0][2] === b[1][1] && b[1][1] === b[2][0] && b[1][1] != " ") {
             return true;
         }
         return false;
-    }
+    };
 
+    // Tie if no cell is empty
     function checkTie() {
         const b = board.getBoard();
         for (let i=0; i<3; i++ ){
             for (let j=0; j<3; j++){
-                if (b[i][j] == "0"){
+                if (b[i][j] == " "){
                     return false;
                 }
             }
@@ -132,19 +131,22 @@ function Gamecontroller() {
         return true;
     }
 
-    function printWinner() {
+    const gameOver = () => {
+        if(checkTie() || checkWinner()){
+            return true;
+        }
+        return false;
+    }
+
+    const printWinner = () => {
         console.log(`The Winner is ${activePlayer.name}`);
     }
 
 
-    return { playRound, getActivePlayer, getBoard: board.getBoard() };
+    return { playRound, getActivePlayer, getBoard: board.getBoard(), gameOver, checkWinner };
 }
 
 
-
-// Display game on webpage
-// Display who's turn it is
-// Display 
 function displayGame() {
     const game = Gamecontroller();
     const turnDiv = document.querySelector(".turn");
@@ -153,7 +155,7 @@ function displayGame() {
 
     function updateScreen() {
         // Clear the board...
-
+        boardDiv.textContent = "";
         // Display who's turn
         turnDiv.textContent = `It's ${game.getActivePlayer().name}'s turn: `;
         
@@ -163,8 +165,8 @@ function displayGame() {
             row.forEach( (cell, c_index) => {
                 const cellBtn = document.createElement("button");
                 cellBtn.className = "cell";
-                cellBtn.setAttribute("data-index", )
-                cellBtn.textContent = cell;
+                cellBtn.setAttribute("data-index", `${r_index} ${c_index}`)
+                cellBtn.textContent = cell; 
                 boardDiv.appendChild(cellBtn);
             });
         });
@@ -172,9 +174,35 @@ function displayGame() {
 
     // Play
     // Use Eventlistener to get change cell
-    boardDiv.addEventListener("click", e => {
-        const currentBtn = e.target
+    boardDiv.addEventListener("click", (e) => {
+        if( !game.gameOver()){
+            const currentBtn = e.target;
+            const [row, col] = currentBtn.dataset.index.split(" ");
+            game.playRound(row, col);
+            updateScreen();
+            if ( game.gameOver()) {
+                displayResult();
+            }
+        }
+
     })
+
+    function displayResult() {
+        const resultDiv = document.querySelector(".result");
+        if (resultDiv.firstElementChild){
+            const prevResult = document.querySelector(".result div");
+            resultDiv.removeChild(prevResult);
+        }
+        const result = document.createElement("div");
+        if (game.checkWinner()) {
+            const activePlayer = game.getActivePlayer().name;
+            result.textContent = `The winner is ${activePlayer}!`;
+        }
+        else {
+            result.textContent = "It's a tie!"
+        }
+        resultDiv.appendChild(result);
+    }
 }
 
 
